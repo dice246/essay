3ec3f97d094764d34beaf96b7fb5351dc43a8b87
yinfan <yinfan@360.cn>
2020-10-16 17:56:13 +0800

    update: 从头到尾打印链表.js

--- 复习/算法/剑指offer/链表/从头到尾打印链表.js
+++ 复习/算法/剑指offer/链表/从头到尾打印链表.js
@@ -1,9 +1,15 @@
 /**
+ * 题目： 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+ * 地址：https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof/
  * Definition for singly-linked list.
  * function ListNode(val) {
  *     this.val = val;
  *     this.next = null;
  * }
+ *
+ * 解题思路：
+ * 1. 遍历链表，插入数组中
+ * 2. 反转数组
  */
 /**
  * @param {ListNode} head
