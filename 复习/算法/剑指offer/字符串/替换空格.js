33b1a593f7635708fdc090e42385b7b40aef10ae
yinfan <yinfan@360.cn>
2020-10-16 17:25:46 +0800

    update: 替换空格

--- 复习/算法/剑指offer/字符串/替换空格.js
+++ 复习/算法/剑指offer/字符串/替换空格.js
@@ -1,9 +1,18 @@
 /**
+ * 题目：替换空格
+ * url: https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/
  * @param {string} s
  * @return {string}
  */
+
+
+/**
+ * 解法一：正则
+ * @param s
+ * @returns {void | string | *}
+ */
 var replaceSpace = function(s) {
-  const regexp = /\s/g;
+  const regexp = / /g;
   const result = s.replace(regexp, '%20')
 
   return result;
